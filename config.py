26d519e82844285acf8d86bf439f8f3e5d0478ce
yesh_naik <[yeshwantnaik12@gmail.com]>
2018-06-04 17:25:45 +0530

    after generic changes

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['yeshwantnaik12@gmail.com']
-    MAILGUN_API_KEY = '8bd85a46e3e3f771a05ea7e570c14b4c-b6183ad4-2682a351'
-    SANDBOX_URL= 'sandboxaf5eede11d354bb089250cd53c2ff34e.mailgun.org'
-    SENDER_EMAIL = 'yeshwant_naik@sandboxaf5eede11d354bb089250cd53c2ff34e.mailgun.org'
-    RECIPIENT_EMAIL = 'yeshwantnaik12@gmail.com'
-    API_KEY = '5c13db10-a9f9-4d67-9fff-f03a0b13288f'
-    DEVICE_ID  = 'BOLT3622826'
+    MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+    SANDBOX_URL= os.environ.get('SANDBOX_URL')    
+    SENDER_EMAIL = os.environ.get('SENDER_EMAIL') 
+    RECIPIENT_EMAIL = os.environ.get('RECIPIENT_EMAIL')
+    API_KEY = os.environ.get('API_KEY')
+    DEVICE_ID  = os.environ.get('DEVICE_ID')
