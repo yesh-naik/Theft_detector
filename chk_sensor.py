9ba21aa070c9c454a7f4bd5578296ba9a89d264f
yesh-naik <yeshwantnaik12@gmail.com>
2019-01-09 10:54:52 +0530

    Update chk_sensor.py

--- chk_sensor.py
+++ chk_sensor.py
@@ -8,10 +8,10 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['yeshwantnaik12@gmail.com']
-    MAILGUN_API_KEY = '8bd85a46e3e3f771a05ea7e570c14b4c-b6183ad4-2682a351'
-    SANDBOX_URL= 'sandboxaf5eede11d354bb089250cd53c2ff34e.mailgun.org'
-    SENDER_EMAIL = 'yeshwant_naik@sandboxaf5eede11d354bb089250cd53c2ff34e.mailgun.org'
-    RECIPIENT_EMAIL = 'yeshwantnaik12@gmail.com'
-    api_key = "5c13db10-a9f9-4d67-9fff-f03a0b13288f"
-    device_id  = "BOLT3622826"
+    ADMINS = ['xxxxxx@xxxx.com']
+    MAILGUN_API_KEY = 'xxxxxxx'
+    SANDBOX_URL= 'xxxxxx'
+    SENDER_EMAIL = 'xxxx@xxxxx.mailgun.org'
+    RECIPIENT_EMAIL = 'xxxx@xxx.com'
+    api_key = "xxxx-xxx-xx-xxx-xxx"
+    device_id  = "BOLTxxxxx"
